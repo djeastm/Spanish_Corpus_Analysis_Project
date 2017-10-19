f0727fc6d0befe844874d6dbceb66e1934afecd6
David Eastman <davidjeastman@gmail.com>
2017-10-18 21:25:52 -0400

    Update web page with more highlighting functionality

--- DE/corpus_html/js/script.js
+++ DE/corpus_html/js/script.js
@@ -1,22 +1,60 @@
-// var sheet = (function() {
-//     // Create the <style> tag
-//     var style = document.createElement("style");
+var tag = '';
 
-//     // Add a media (and/or media query) here if you'd like!
-//     // style.setAttribute("media", "screen")
-//     // style.setAttribute("media", "only screen and (max-width : 1024px)")
+function highlight() {
+    clearHighlight(); // Turn off all highlighting
+    pos = document.getElementById("POS").value;
+    //console.log(pos);
+    tag = pos;
+    
+    var selects = document.querySelectorAll("[type=\""+pos+"\"]"); 
+    
+    for (var i = 0; i < selects.length; i++) {
+        console.log(selects[i].selectedIndex);
+        ind = selects[i].selectedIndex;                
+        val = selects[i].options[ind].value;
+        if (val=="0") break;
+        tag += val;        
+    }
 
-//     // WebKit hack :(
-//     style.appendChild(document.createTextNode(""));
+    var count = highlightTaggedWords();
+    document.getElementById("word_count").innerHTML = "Word count:"+count;
 
-//     // Add the <style> element to the page
-//     document.head.appendChild(style);
+    event.preventDefault(); // disable normal form submit behavior
 
-//     return style.sheet;
-// })();
+    return false; // prevent further bubbling of event
+}
 
-// sheet.insertRule("[t^=\"V\"] {background-color: blue;}", 1);
-var words = document.querySelectorAll("[t^=\"V\"]");
-for (var i = 0; i < words.length; i++) {
-    words[i].style.backgroundColor="blue";
-};
+function showHideSelects() {
+    pos = document.getElementById("POS").value;
+
+    // Make this type select boxes visible and hide other types
+    var selects = document.querySelectorAll("select, label");    
+    for (var i = 0; i < selects.length; i++) {
+        selects[i].style.display="none";
+    };
+
+    var selects = document.querySelectorAll("[class=\""+pos+"\"]");    
+    for (var i = 0; i < selects.length; i++) {
+        selects[i].style.display="inline-block";
+    };
+
+    document.getElementById("POS").style.display="block"
+}
+
+function highlightTaggedWords() {
+    var this_tag = "[t^=\""+tag+"\"]"
+    
+    var words = document.querySelectorAll(this_tag);
+    for (var i = 0; i < words.length; i++) {
+        words[i].style.backgroundColor="blue";
+    };
+    tag = ''; 
+    return words.length;   
+}
+
+function clearHighlight() {
+    var words = document.querySelectorAll("w");
+    for (var i = 0; i < words.length; i++) {
+        words[i].style.backgroundColor="black";
+    };
+}
