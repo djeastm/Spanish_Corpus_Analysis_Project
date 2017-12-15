3bd92ea8f6354b1abe18b900b341ab2c90aad8e3
David Eastman <davidjeastman@gmail.com>
2017-12-15 12:01:20 -0500

    Create IndexedDB/Dexie NoSQL database of PRESEEA corpus using JSON data store

--- DE/PRESEEA/build_PRESEEA_corpus.py
+++ DE/PRESEEA/build_PRESEEA_corpus.py
@@ -7,53 +7,68 @@ import os
 import xmltodict
 import json
 from flatten_json import flatten
-#from es_parser import parser
 
 def main():
-    path = 'IndividualTextsArchive/'
-
-##    htmloutput = "<div id=\"corpus\" class=\"column middle\">\n"
-    count = 0
-
-    for filename in os.listdir(path):
-        with open(path+filename,encoding='ISO-8859-1') as fp:
-            if count == 1:
-                break
+    with open("corpus_preseea.json",'a') as f:
+        f.write("[")
+    with open("raw_corpus.txt",encoding='utf-8') as fp:
+        corpus = fp.read()
+        texts = corpus.split("|TEXTSEPARATOR|")
+        count = 0
+        schema = []
+        for text in texts:
+            if len(text) < 10: continue # ignore last split
             count = count + 1
-            if filename.startswith("_"):
-                filename = filename[1:]
-            print(filename)
-            raw_text = fp.read()
-            end_header_index = raw_text.find("</Trans>") # select header information only
-            
-            soup = BeautifulSoup(raw_text[:end_header_index+8], 'lxml-xml')
+##            if count == 5:
+##                break
+        
+##            print(str(raw_text))
+            end_header_index = text.find("</Trans>") # select header information only
+            header = text[:end_header_index+8]            
+            soup = BeautifulSoup(header, 'lxml-xml')
             text_tag = soup.new_tag("text")
-            text_tag.string = raw_text[end_header_index+8:] #put rest of text in its own tag
-            text = soup.Trans
-##            text.append(text_tag)
-##            id_tag = soup.new_tag("id")
-##            id_tag.string = str(count)
-##            text.append(id_tag)
-            d = xmltodict.parse(str(text))
-            flat = flatten(d)
-##            print(flat)
-            # For Schema (replace @ symbol with one that can be used in keypath)
-            for k, v in flat.items():                
-                flat[k] = v.replace("'","")
-                old = k
-                new = k.replace("@","DE_")
-                flat[new] = flat.pop(old)
-                flat[new.replace("Trans_","")] = flat.pop(new)
-
-            for k, v in flat.items():
-                print(k+',', end="")
+            text_tag.string = text[end_header_index+8:] #put rest of text in its own tag
+            text_name = soup.Datos['clave_texto']
+            print(text_name.strip())
+            text_xml = soup.Trans
+    ##            text.append(text_tag)
+    ##            id_tag = soup.new_tag("id")
+    ##            id_tag.string = str(count)
+    ##            text.append(id_tag)
             
+            try:
+                d = xmltodict.parse(str(text_xml))
+                flat = flatten(d)
+    ##            print(flat)
+                flat_clean = {}
+                for k, v in flat.items():      
+                    k = k.strip()
+                    k = k.replace("@","")
+                    k = k.replace(":","_")
+                    k = k.replace("#","")
+                    k = k.replace("Trans_","")
+                    v = v.strip()
+                    v = v.replace("'","")                                   
+                    flat_clean[k] = v
+                    if k not in schema:
+                        schema.append(k)
             
-            json.dump(flat, open("test_2.json",'w'))
-##            json_str = json.dumps(d)
+                if count != 1: #separate texts with comma
+                    with open("corpus_preseea.json",'a') as f:
+                        f.write(",")
+                json.dump(flat_clean, open("corpus_preseea.json",'a'))
+            except:                
+                print("Expat Error in "+count)
+        for key in schema:
+            print(key+',', end="")
+    with open("corpus_preseea.json",'a') as f:
+        f.write("]")
+    
             
-##            with open("CorpusPRESEEA.html",'w', encoding='utf-8') as corpus:
-##                corpus.write(htmloutput)
+    ##            json_str = json.dumps(d)
+            
+    ##            with open("CorpusPRESEEA.html",'w', encoding='utf-8') as corpus:
+    ##                corpus.write(htmloutput)
 
 if __name__ == "__main__":
     main()                                                        
--- DE/webpage/js/script_preseea.js
+++ DE/webpage/js/script_preseea.js
@@ -1,40 +1,53 @@
 import Dexie from "Dexie";
 import IDBExportImport from "indexeddb-export-import";
 
+var corpus;
+var request_corpus = new XMLHttpRequest();
+
 var db = new Dexie("PRESEEA");
 Dexie.debug = true;
-// db.version(1).stores({
-//     texts : "id++, Trans, text"
-// });
 db.version(1).stores({
-    texts: "++id, Datos_Corpus_DE_ciudad,Datos_Corpus_DE_corpus,Datos_Corpus_DE_pais,Datos_Corpus_DE_subcorpus,Datos_DE_clave_texto,Datos_DE_tipo_texto,Datos_Grabacion_DE_duracion,Datos_Grabacion_DE_fecha_grab,Datos_Grabacion_DE_lugar,Datos_Grabacion_DE_resp_grab,Datos_Transcripcion_DE_numero_palabras,Datos_Transcripcion_DE_resp_trans,Hablantes_Hablante_0_DE_codigo_hab,Hablantes_Hablante_0_DE_edad,Hablantes_Hablante_0_DE_estudios,Hablantes_Hablante_0_DE_grupo_edad,Hablantes_Hablante_0_DE_id,Hablantes_Hablante_0_DE_nivel_edu,Hablantes_Hablante_0_DE_origen,Hablantes_Hablante_0_DE_papel,Hablantes_Hablante_0_DE_profesion,Hablantes_Hablante_0_DE_sexo,Hablantes_Hablante_1_DE_codigo_hab,Hablantes_Hablante_1_DE_edad,Hablantes_Hablante_1_DE_estudios,Hablantes_Hablante_1_DE_grupo_edad,Hablantes_Hablante_1_DE_id,Hablantes_Hablante_1_DE_nivel_edu,Hablantes_Hablante_1_DE_origen,Hablantes_Hablante_1_DE_papel,Hablantes_Hablante_1_DE_profesion,Hablantes_Hablante_1_DE_sexo,Hablantes_Relaciones_DE_rel_ent_aud1,Hablantes_Relaciones_DE_rel_ent_aud2,Hablantes_Relaciones_DE_rel_ent_inf,Hablantes_Relaciones_DE_rel_inf_aud1,Hablantes_Relaciones_DE_rel_inf_aud2"
+    texts: "++id, Datos_Corpus_pais,Hablantes_Relaciones_rel_ent_aud1,Hablantes_Hablante_1_codigo_hab,Hablantes_Hablante_0_edad,Hablantes_Hablante_0_nombre,Datos_Grabacion_resp_grab,Hablantes_Hablante_0_estudios,Hablantes_Hablante_1_sexo,Hablantes_Hablante_0_papel,Datos_Transcripcion_numero_palabras,Datos_Revision_0_num_rev,Hablantes_Relaciones_rel_inf_aud2,Datos_Revision_1_resp_rev,Datos_clave_texto,Hablantes_Hablante_1_papel,Datos_Grabacion_duracion,Hablantes_Hablante_0_codigo_hab,Hablantes_Hablante_0_origen,Hablantes_Hablante_0_nivel_edu,Datos_Corpus_ciudad,Hablantes_Relaciones_rel_ent_aud2,Datos_Revision_0_fecha_rev,Datos_Revision_1_num_rev,Datos_tipo_texto,Datos_Grabacion_sistema,Hablantes_Relaciones_rel_inf_aud1,Datos_Revision_0_resp_rev,Datos_Revision_1_fecha_rev,xml_lang,Hablantes_Hablante_1_edad,Hablantes_Hablante_0_sexo,Hablantes_Relaciones_rel_ent_inf,Datos_Transcripcion_resp_trans,Datos_Corpus_subcorpus,Hablantes_Hablante_0_grupo_edad,Datos_Grabacion_lugar,Hablantes_Hablante_1_id,Hablantes_Hablante_1_nombre,Datos_Transcripcion_fecha_trans,audio_filename,Hablantes_Hablante_1_estudios,Hablantes_Hablante_1_origen,Hablantes_Hablante_1_grupo_edad,Hablantes_Hablante_0_profesion,Hablantes_Hablante_1_profesion,Datos_Grabacion_fecha_grab,Datos_Corpus_corpus,Hablantes_Hablante_1_nivel_edu,Hablantes_Hablante_0_id,Hablantes_Hablante_2_origen,Hablantes_Hablante_2_codigo_hab,Hablantes_Hablante_2_sexo,Hablantes_Hablante_2_estudios,Hablantes_Hablante_2_grupo_edad,Hablantes_Hablante_2_profesion,Hablantes_Hablante_2_papel,Hablantes_Hablante_2_nombre,Hablantes_Hablante_2_nivel_edu,Hablantes_Hablante_2_id,Hablantes_Hablante_2_edad,Hablantes_text,Hablantes_Hablante_0_profesión,Datos_Revision_2_fecha_rev,Datos_Revision_2_num_rev,Datos_Revision_2_resp_rev,Datos_Grabacion_duración,Datos_Revision_4_num_rev,Datos_Revision_4_fecha_rev,Datos_Revision_3_fecha_rev,Datos_Revision_3_resp_rev,Datos_Revision_3_num_rev,Datos_Revision_4_resp_rev,Hablantes_Hablante_9_id,Hablantes_Hablante_7_estudios,Hablantes_Hablante_3_papel,Hablantes_Hablante_6_estudios,Hablantes_Hablante_6_codigo_hab,Hablantes_Hablante_12_nivel_edu,Hablantes_Hablante_13_edad,Hablantes_Hablante_6_nombre,Hablantes_Hablante_7_edad,Hablantes_Hablante_10_nombre,Hablantes_Hablante_3_estudios,Hablantes_Hablante_14_estudios,Hablantes_Hablante_4_grupo_edad,Hablantes_Hablante_4_edad,Hablantes_Hablante_12_edad,Hablantes_Hablante_4_papel,Hablantes_Hablante_5_profesion,Hablantes_Hablante_5_origen,Hablantes_Hablante_13_grupo_edad,Hablantes_Hablante_14_sexo,Hablantes_Hablante_9_nombre,Hablantes_Hablante_13_nombre,Hablantes_Hablante_3_origen,Hablantes_Hablante_7_nivel_edu,Hablantes_Hablante_11_papel,Hablantes_Hablante_8_estudios,Hablantes_Hablante_4_estudios,Hablantes_Hablante_9_papel,Hablantes_Hablante_12_nombre,Hablantes_Hablante_12_id,Hablantes_Hablante_5_estudios,Hablantes_Hablante_5_codigo_hab,Hablantes_Hablante_3_sexo,Hablantes_Hablante_8_papel,Hablantes_Hablante_6_id,Hablantes_Hablante_7_origen,Hablantes_Hablante_8_sexo,Hablantes_Hablante_13_papel,Hablantes_Hablante_3_edad,Hablantes_Hablante_11_codigo_hab,Hablantes_Hablante_7_papel,Hablantes_Hablante_13_estudios,Hablantes_Hablante_11_sexo,Hablantes_Hablante_10_codigo_hab,Hablantes_Hablante_10_estudios,Hablantes_Hablante_4_codigo_hab,Hablantes_Hablante_3_grupo_edad,Hablantes_Hablante_8_id,Hablantes_Hablante_11_origen,Hablantes_Hablante_12_grupo_edad,Hablantes_Hablante_5_nombre,Hablantes_Hablante_13_id,Hablantes_Hablante_11_nombre,Hablantes_Hablante_3_nombre,Hablantes_Hablante_13_codigo_hab,Hablantes_Hablante_9_grupo_edad,Hablantes_Hablante_4_id,Hablantes_Hablante_11_nivel_edu,Hablantes_Hablante_4_nombre,Hablantes_Hablante_8_profesion,Hablantes_Hablante_9_edad,Hablantes_Hablante_8_codigo_hab,Hablantes_Hablante_11_estudios,Hablantes_Hablante_11_profesion,Hablantes_Hablante_9_origen,Hablantes_Hablante_5_nivel_edu,Hablantes_Hablante_6_papel,Hablantes_Hablante_5_grupo_edad,Hablantes_Hablante_14_grupo_edad,Hablantes_Hablante_13_origen,Hablantes_Hablante_5_sexo,Hablantes_Hablante_3_codigo_hab,Hablantes_Hablante_9_profesion,Hablantes_Hablante_6_grupo_edad,Hablantes_Hablante_3_id,Hablantes_Hablante_14_profesion,Hablantes_Hablante_13_sexo,Hablantes_Hablante_3_nivel_edu,Hablantes_Hablante_10_nivel_edu,Hablantes_Hablante_5_id,Hablantes_Hablante_9_nivel_edu,Hablantes_Hablante_5_papel,Hablantes_Hablante_10_grupo_edad,Hablantes_Hablante_10_sexo,Hablantes_Hablante_14_nivel_edu,Hablantes_Hablante_14_origen,Hablantes_Hablante_7_codigo_hab,Hablantes_Hablante_3_profesion,Hablantes_Hablante_6_origen,Hablantes_Hablante_6_edad,Hablantes_Hablante_13_nivel_edu,Hablantes_Hablante_12_papel,Hablantes_Hablante_9_estudios,Hablantes_Hablante_7_sexo,Hablantes_Hablante_6_nivel_edu,Hablantes_Hablante_6_profesion,Hablantes_Hablante_4_sexo,Hablantes_Hablante_12_codigo_hab,Hablantes_Hablante_7_grupo_edad,Hablantes_Hablante_11_id,Hablantes_Hablante_14_papel,Hablantes_Hablante_14_id,Hablantes_Hablante_14_nombre,Hablantes_Hablante_6_sexo,Hablantes_Hablante_14_edad,Hablantes_Hablante_8_nivel_edu,Hablantes_Hablante_8_nombre,Hablantes_Hablante_10_papel,Hablantes_Hablante_9_sexo,Hablantes_Hablante_14_codigo_hab,Hablantes_Hablante_12_estudios,Hablantes_Hablante_11_grupo_edad,Hablantes_Hablante_10_edad,Hablantes_Hablante_11_edad,Hablantes_Hablante_8_edad,Hablantes_Hablante_9_codigo_hab,Hablantes_Hablante_8_origen,Hablantes_Hablante_5_edad,Hablantes_Hablante_4_origen,Hablantes_Hablante_7_nombre,Hablantes_Hablante_4_nivel_edu,Hablantes_Hablante_4_profesion,Hablantes_Hablante_10_id,Hablantes_Hablante_12_origen,Hablantes_Hablante_12_profesion,Hablantes_Hablante_10_origen,Hablantes_Hablante_13_profesion,Hablantes_Hablante_7_id,Hablantes_Hablante_8_grupo_edad,Hablantes_Hablante_12_sexo,Hablantes_Hablante_10_profesion,Hablantes_Hablante_7_profesion,Hablantes_Relaciones_rel_ent_aud3,Hablantes_Relaciones_rel_inf_aud3,Hablantes_Relaciones_rel_ent_inf2,Hablantes_Relaciones_rel_inf_inf2,Hablantes_Relaciones_rel_inf_aud4,Hablantes_Relaciones_rel_ent_aud4,Hablantes_Hablante_1_profesión,Hablantes_Hablante_1_código_hab"
 });
-// db.version(1).stores({
-//     texts: "++id, Trans_Hablantes_Hablante_1_DE_papel,Trans_Datos_DE_tipo_texto,Trans_Datos_Transcripcion_DE_numero_palabras,Trans_Datos_Revision_1_DE_num_rev,Trans_Hablantes_Hablante_1_DE_grupo_edad,Trans_Hablantes_Hablante_1_DE_nombre,Trans_Datos_DE_clave_texto,Trans_Hablantes_Hablante_1_DE_profesion,Trans_Hablantes_Hablante_0_DE_estudios,Trans_Hablantes_Hablante_0_DE_papel,Trans_Hablantes_Hablante_0_DE_origen,Trans_Hablantes_Hablante_0_DE_sexo,Trans_Hablantes_Relaciones_DE_rel_inf_aud2,Trans_Hablantes_Relaciones_DE_rel_ent_aud2,Trans_Datos_Revision_0_DE_fecha_rev,Trans_Datos_Grabacion_DE_lugar,Trans_Datos_Revision_0_DE_num_rev,Trans_Datos_Revision_0_DE_resp_rev,Trans_Hablantes_Hablante_1_DE_nivel_edu,Trans_Hablantes_Hablante_1_DE_estudios,Trans_Hablantes_Hablante_1_DE_origen,Trans_Hablantes_Hablante_1_DE_sexo,Trans_Datos_Transcripcion_DE_fecha_trans,Trans_Datos_Grabacion_DE_resp_grab,Trans_Datos_Revision_1_DE_fecha_rev,Trans_Hablantes_Hablante_0_DE_nivel_edu,Trans_Hablantes_Hablante_0_DE_edad,Trans_Datos_Revision_1_DE_resp_rev,Trans_Datos_Transcripcion_DE_resp_trans,Trans_Hablantes_Relaciones_DE_rel_ent_inf,Trans_Hablantes_Hablante_0_DE_profesion,Trans_Datos_Grabacion_DE_sistema,Trans_DE_xml:lang,Trans_Datos_Corpus_DE_subcorpus,Trans_Hablantes_Hablante_0_DE_codigo_hab,Trans_Datos_Corpus_DE_ciudad,Trans_Hablantes_Hablante_0_DE_grupo_edad,Trans_Hablantes_Hablante_1_DE_edad,Trans_Hablantes_Relaciones_DE_rel_inf_aud1,Trans_Hablantes_Hablante_0_DE_nombre,Trans_Hablantes_Relaciones_DE_rel_ent_aud1,Trans_Datos_Grabacion_DE_duracion,Trans_Hablantes_Hablante_1_DE_codigo_hab,Trans_DE_audio_filename,Trans_Hablantes_Hablante_0_DE_id,Trans_Datos_Corpus_DE_corpus,Trans_Datos_Corpus_DE_pais,Trans_Datos_Grabacion_DE_fecha_grab,Trans_Hablantes_Hablante_1_DE_id"
-// });
-
 
 db.on("ready", function() {
-    console.log("Hi:");
-    var idb_db = db.backendDB(); // get native IDBDatabase object from Dexie wrapper
+    console.log("Request Corpus");    
+    var requestURL_corpus = 'corpus_preseea.json';
+    request_corpus.open('GET', requestURL_corpus);
+    request_corpus.responseType = 'json';
+    request_corpus.send();
 
-    // export to JSON, clear database, and import from JSON
-    IDBExportImport.exportToJsonString(idb_db, function(err, jsonString) {
-        if(err)
-            console.error(err);
-        else {
-            // console.log("Exported as JSON: " + jsonString);
-            jsonString = '{"texts":[{"Hablantes_Hablante_1_DE_nivel_edu": "alto", "Hablantes_Relaciones_DE_rel_ent_inf": "desconocidos", "Datos_Transcripcion_DE_resp_trans": "Equipo de Alcal\u00e1", "Hablantes_Hablante_0_DE_nivel_edu": "bajo", "Datos_Revision_0_DE_resp_rev": "responsable", "Hablantes_Hablante_1_DE_papel": "entrevistador", "Hablantes_Hablante_1_DE_profesion": "profesor", "Datos_Corpus_DE_pais": "Espa\u00f1a", "Datos_Revision_1_DE_resp_rev": "responsable", "Hablantes_Hablante_0_DE_estudios": "graduado escolar", "DE_audio_filename": "ALCA_H11_037.mp3", "Hablantes_Relaciones_DE_rel_ent_aud1": "no", "Hablantes_Hablante_1_DE_grupo_edad": "2", "Hablantes_Hablante_0_DE_profesion": "dependiente de comercio", "Datos_Corpus_DE_corpus": "PRESEEA", "Datos_Revision_0_DE_fecha_rev": "aaaa-mm-dd", "Hablantes_Hablante_1_DE_edad": "desc", "Hablantes_Relaciones_DE_rel_inf_aud1": "no", "Datos_DE_clave_texto": "ALCA_H11_037", "Datos_DE_tipo_texto": "entrevista_semidirigida", "Datos_Grabacion_DE_resp_grab": "Equipo de Alcal\u00e1", "Hablantes_Hablante_1_DE_nombre": "Florentino Paredes", "Hablantes_Hablante_1_DE_origen": "desc", "Hablantes_Hablante_1_DE_estudios": "filolog\u00eda", "Datos_Grabacion_DE_duracion": "5000", "Hablantes_Hablante_0_DE_origen": "Guadalajara", "Datos_Transcripcion_DE_numero_palabras": "10684", "Hablantes_Hablante_0_DE_edad": "33", "Datos_Grabacion_DE_fecha_grab": "1998-06-25", "Hablantes_Relaciones_DE_rel_ent_aud2": "no", "Hablantes_Relaciones_DE_rel_inf_aud2": "no", "Hablantes_Hablante_0_DE_id": "hab1", "Hablantes_Hablante_0_DE_codigo_hab": "I", "Hablantes_Hablante_0_DE_nombre": " ALCA_H11_037", "Datos_Grabacion_DE_lugar": "desc", "Hablantes_Hablante_0_DE_grupo_edad": "1", "Hablantes_Hablante_1_DE_id": "hab2", "Datos_Revision_0_DE_num_rev": "1", "DE_xml:lang": "espa\u00f1ol", "Hablantes_Hablante_0_DE_papel": "informante", "Datos_Transcripcion_DE_fecha_trans": "2011-03-23", "Datos_Grabacion_DE_sistema": "MP3", "Hablantes_Hablante_1_DE_sexo": "hombre", "Datos_Revision_1_DE_fecha_rev": "aaaa-mm-dd", "Hablantes_Hablante_1_DE_codigo_hab": "E", "Datos_Corpus_DE_ciudad": "Alcal\u00e1 de Henares", "Hablantes_Hablante_0_DE_sexo": "hombre", "Datos_Revision_1_DE_num_rev": "2", "Datos_Corpus_DE_subcorpus": "Alcal\u00e1"}]}';
-            //console.log("String: " + jsonString);
-            IDBExportImport.clearDatabase(idb_db, function(err) {                
-                if(!err) // cleared data successfully                    
-                    IDBExportImport.importFromJsonString(idb_db, jsonString, function(err) {
-                        //console.log("Imported data: " + jsonString);
-                        if (!err)
-                            console.log("Imported data successfully");
-                    });
-            });
-        }
-    });
+    
 });
+
 db.open();
+
+request_corpus.onreadystatechange = function() {
+    if (request_corpus.readyState === 4) {
+      //console.log(request_corpus.response); //Outputs a DOMString by default
+      corpus = request_corpus.response;
+      //console.log(JSON.stringify(corpus));
+      //console.log(corpus);
+      var idb_db = db.backendDB(); // get native IDBDatabase object from Dexie wrapper
+
+        // export to JSON, clear database, and import from JSON
+        IDBExportImport.exportToJsonString(idb_db, function(err, jsonString) {
+            if(err)
+                console.error(err);
+            else {
+                
+            // console.log("Exported as JSON: " + jsonString);
+                jsonString = '{"texts":'+JSON.stringify(corpus)+'}';
+                //console.log("String: " + jsonString);
+                IDBExportImport.clearDatabase(idb_db, function(err) {                
+                    if(!err) // cleared data successfully                    
+                        IDBExportImport.importFromJsonString(idb_db, jsonString, function(err) {
+                            //console.log("Imported data: " + jsonString);
+                            if (!err)
+                                console.log("Imported data successfully");
+                        });
+                });
+            }
+        });      
+    }
+  }
